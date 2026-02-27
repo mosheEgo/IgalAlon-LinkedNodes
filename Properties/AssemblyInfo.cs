1f3cbe7b438e5d49f9b7599dc85279a13fb399b1
Moshe Ego <moshe.ego@gmail.com>
2026-02-27 19:31:33 +0200

    Fix repository structure (flatten folders) + restore AssemblyInfo

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Bagrut2025_271_LinkedNodes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bagrut2025_271_LinkedNodes")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible
+[assembly: ComVisible(false)]
+
+// GUID - אפשר להשאיר ככה או לשים GUID חדש
+[assembly: Guid("d29ff8f8-06a4-40ac-9ab8-424f4f1363ef")]
+
+// Version
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
